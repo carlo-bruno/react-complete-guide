0f0b2609af9623ad644b26b0ec0e58ed7303e8f4
Carlo Bruno <carlofbruno@gmail.com>
2018-08-08 17:36:12 -0700

    initial commit
    Udemy Course: React 16 -  Maximilian Schwarzmüller

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Udemy React 16 -  Maximilian Schwarzmüller </h1>
+        <h1>Udemy Course: React 16 -  Maximilian Schwarzmüller </h1>
       </div>
     );
   }
