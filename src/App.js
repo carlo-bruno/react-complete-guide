a49c3403eebc3221efd2ebf7135bb617ea928623
Carlo Bruno <carlofbruno@gmail.com>
2018-08-08 21:07:11 -0700

    lecture 42

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons : [
+      { name: "Joe", age: 28 },
+      { name: "Jack", age: 32 },
+      { name: "Rachel", age: 22 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    // console.log('button clicked');
+    // ! WRONG: this.state.persons[0].name = "Joseph";
+    // * RIGHT: use setState()
+    this.setState( { persons: [ 
+      { name: "Joseph", age: 28 },
+      { name: "Jack", age: 32 },
+      { name: "Rachel", age: 23 }
+    ]
+  } )
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Udemy Course: React 16 -  Maximilian Schwarzmüller </h1>
-        <Person name="Joe" age="28" />
-        <Person name="Jack" age ="32">My Hobbies: Lumberjacking </Person>
-        <Person name="Rachel" age="22"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
+        <Person name={ this.state.persons[1].name } age ={ this.state.persons[1].age }>My Hobbies: Lumberjacking </Person>
+        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age }/>
       </div>
     );
-  }
+  };
 }
 
 export default App;
