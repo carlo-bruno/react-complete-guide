79df09451c237c19e9f53cce6b9a31f153fad38b
Carlo Bruno <carlofbruno@gmail.com>
2018-08-08 17:32:53 -0700

    initial commit
    Udemy Course: React 16 -  Maximilian Schwarzmüller

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Udemy React 16 -  Maximilian Schwarzmüller </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
