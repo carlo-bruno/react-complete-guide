87bde324908b082712bd92dda78c35ce67971cad
Carlo Bruno <carlofbruno@gmail.com>
2018-08-08 18:59:40 -0700

    lecture 34

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Udemy Course: React 16 -  Maximilian Schwarzmüller </h1>
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
   }
