7919f28ec5218bc9e04145bb43f0c11d919fb894
Carlo Bruno <carlofbruno@gmail.com>
2018-08-08 19:02:42 -0700

    lecture 35

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = () => {
-    return <p>Im a Person!</p>
+    return <p>Im a Person and I am { Math.floor(Math.random() *30) } years old!!</p>
 };
 
 export default person;
