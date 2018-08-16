51807710d60105c0255023537c2b7331ce318258
Carlo Bruno <carlofbruno@gmail.com>
2018-08-15 18:12:42 -0700

    lecture 83
    converting stateless (functional) components to stateful components

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return <ErrorBoundary key={person.id}>
-      <Person
-        click={() => props.clicked(index)}
-        name={person.name}  //! create props that is then called in the App.js
-        age={person.age}    //! using {this.} 
-        changed={(event) => props.changed(event, person.id)} /> 
-      </ErrorBoundary>
-  })
+class Persons extends Component {
+  render () {
+    return this.props.persons.map((person, index) => {
+      return <ErrorBoundary key={person.id}>
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}  
+          changed={(event) => this.props.changed(event, person.id)} /> 
+        </ErrorBoundary>
+    })
+  }
 
-export default persons;
+}
+
+export default Persons;
